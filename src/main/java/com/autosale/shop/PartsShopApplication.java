4573a59b5ac2575f709eede9a05b73d2c16ea2be
Dex <maroderko8@gmail.com>
2023-03-10 14:55:55 +0200

    Initial commit

---
+++ src/main/java/com/autosale/shop/PartsShopApplication.java
@@ -0,0 +1,13 @@
+package com.autosale.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartsShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartsShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autosale/shop/PartsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autosale.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

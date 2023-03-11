07d7c2ea6e34d121a137bf29b4fd2a313f3e42b0
Dex <maroderko8@gmail.com>
2023-03-11 10:40:36 +0200

    Wrapped controller return in ResponseEntity with status codes

--- src/main/java/com/autosale/shop/controller/UserController.java
+++ src/main/java/com/autosale/shop/controller/UserController.java
@@ -2,7 +2,11 @@ package com.autosale.shop.controller;
 
 import com.autosale.shop.model.User;
 import com.autosale.shop.service.UserService;
+import org.jooq.exception.DataAccessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,6 +14,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
 
     private final UserService userService;
 
@@ -24,26 +30,50 @@ public class UserController {
     }
 
     @PostMapping("/create")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Integer create(@RequestBody User user)
+    public ResponseEntity<Integer> create(@RequestBody User user)
     {
-        return userService.create(user);
+        try {
+            return ResponseEntity.ok(userService.create(user));
+        }
+        catch (DataAccessException e)
+        {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
     }
 
     @GetMapping("/get/{id}")
-    public User findById(@PathVariable int id){
-        return userService.findById(id);
+    public ResponseEntity<User> findById(@PathVariable int id){
+        try {
+            return ResponseEntity.ok(userService.findById(id));
+        }
+        catch (DataAccessException e)
+        {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
     @PostMapping("/delete/{id}")
-    public int delete(@PathVariable int id)
+    public ResponseEntity<Integer> delete(@PathVariable int id)
     {
-        return userService.delete(id);
+        try {
+            return ResponseEntity.ok(userService.delete(id));
+        }
+        catch (DataAccessException e)
+        {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
     @PostMapping("/update")
-    public int update(@RequestBody User user)
+    public ResponseEntity<Integer> update(@RequestBody User user)
     {
-        return userService.edit(user);
+        try {
+            return ResponseEntity.ok(userService.edit(user));
+        }
+        catch (DataAccessException e)
+        {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }

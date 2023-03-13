0b084c51be3b977efebdca8d8935e1c2bb55ed9f
Dex <maroderko8@gmail.com>
2023-03-13 17:43:47 +0200

    Changed @Data to its immutable version @Value

--- src/main/java/com/autosale/shop/model/User.java
+++ src/main/java/com/autosale/shop/model/User.java
@@ -1,11 +1,11 @@
 package com.autosale.shop.model;
 
-import lombok.Data;
+import lombok.Value;
 
-@Data
+@Value
 public class User {
-    private final Integer id;
-    private final String userName;
-    private final String password;
-    private final UserRole role;
+    Integer id;
+    String userName;
+    String password;
+    UserRole role;
 }

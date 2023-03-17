18f930da5d8c8f59a4862b3d53b2038e85957dc8
Dex <maroderko8@gmail.com>
2023-03-17 11:40:35 +0200

    Changed UserGenerator algorithm

--- src/test/java/com/autosale/shop/generator/UserGenerator.java
+++ src/test/java/com/autosale/shop/generator/UserGenerator.java
@@ -5,11 +5,12 @@ import com.autosale.shop.model.UserRole;
 import net.bytebuddy.utility.RandomString;
 
 import java.util.Random;
+import java.util.UUID;
 
 public class UserGenerator {
     private static final Random rand = new Random();
     public static User generate()
     {
-        return new User(rand.nextInt(100), RandomString.make(50), RandomString.make(25), rand.nextBoolean() ? UserRole.USER : UserRole.ADMIN);
+        return new User(rand.nextInt(100), UUID.randomUUID().toString(), UUID.randomUUID().toString(), rand.nextBoolean() ? UserRole.USER : UserRole.ADMIN);
     }
 }

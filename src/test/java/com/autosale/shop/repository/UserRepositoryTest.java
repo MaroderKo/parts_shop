6691b78d61c6be1b7ab946f6c112e16254b5a21f
Dex <maroderko8@gmail.com>
2023-03-23 17:59:46 +0200

    Some code refactoring

--- src/test/java/com/autosale/shop/repository/UserRepositoryTest.java
+++ src/test/java/com/autosale/shop/repository/UserRepositoryTest.java
@@ -3,11 +3,10 @@ package com.autosale.shop.repository;
 import com.autosale.shop.configuration.TestBeanFactory;
 import com.autosale.shop.model.User;
 import com.autosale.shop.repository.impl.UserRepositoryImpl;
+import org.apache.naming.factory.BeanFactory;
 import org.jooq.impl.DSL;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
+import structure.tables.Users;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,25 +16,36 @@ import static com.autosale.shop.generator.UserGenerator.generate;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
 public class UserRepositoryTest {
-    UserRepository repository = new UserRepositoryImpl(TestBeanFactory.testDSLContext());
+    private final UserRepository repository = new UserRepositoryImpl(TestBeanFactory.testDSLContext());
+
+    static User user;
+
+    @AfterEach
+    void clear()
+    {
+        TestBeanFactory.testDSLContext().delete(Users.USERS).execute();
+    }
+
+    @BeforeEach
+    void setUp()
+    {
+        user = generate();
+    }
 
     @Test
-    @Order(1)
     void findAll() {
-        User user = generate();
         User user1 = generate();
         User user2 = generate();
         repository.save(user);
         repository.save(user1);
         repository.save(user2);
-        assertThat(repository.findAll(), hasItems(user, user1, user2));
+        assertThat(repository.findAll(), containsInAnyOrder(user, user1, user2));
     }
 
     @Test
     void findById() {
-        User user = generate();
         repository.save(user);
         assertThat(repository.findById(user.getId()).isPresent(), is(true));
         assertThat(repository.findById(user.getId()).get(), is(user));
@@ -44,15 +54,13 @@ public class UserRepositoryTest {
 
     @Test
     void save() {
-        User user = generate();
         Optional<Integer> save = repository.save(user);
         assertThat(save.isPresent(), is(true));
-        assertThat(save.get(), is(user.getId()));
+        assertThat(repository.findById(save.get()).get(), is(user));
     }
 
     @Test
     void update() {
-        User user = generate();
         repository.save(user);
 
         User newUser = new User(user.getId(), "ChangedUsername", user.getPassword(), user.getRole());
@@ -63,10 +71,10 @@ public class UserRepositoryTest {
 
     @Test
     void deleteById() {
-        User user = generate();
         repository.save(user);
 
         int i = repository.deleteById(user.getId());
         assertThat(i, is(1));
+        assertThat(repository.findById(user.getId()).isPresent(), is(false));
     }
 }

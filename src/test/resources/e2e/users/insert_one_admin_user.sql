7ed7d828ee5a275de4161de02fcfa0388fe035d1
Dex <maroderko8@gmail.com>
2023-05-03 18:32:28 +0300

    Fixed User table truncate restriction error

--- src/test/resources/e2e/users/insert_one_admin_user.sql
+++ src/test/resources/e2e/users/insert_one_admin_user.sql
@@ -1,3 +1,3 @@
-truncate users;
+truncate users cascade;
 insert into users(id, user_name, password, role)
 VALUES (2, 'admin', '$2a$12$sqy8znpp2RQedfIc/PT3JOqVFz5GuHV1eQstTRuOz3tJYkHocW.yS', 'ADMIN');
